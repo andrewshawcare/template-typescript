ec32acb6b163406d6c3ed67c099d0e83c5bdd7b0
Andrew Shaw Care <Andrew Shaw Care>
2023-06-12 14:56:31 -0400

    Remove .ts extension from import

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import assert from "assert";
 
-import { generateTruth } from "./generate-truth.ts";
+import { generateTruth } from "./generate-truth";
 
 describe("generateTruth", () => {
     it("should be a function that returns true", () => {
